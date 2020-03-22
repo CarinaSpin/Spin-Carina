4a13f31fa4a0b43d99c69f2768580dd1a3bbda52
CarinaSpin <61678323+CarinaSpin@users.noreply.github.com>
2020-03-22 21:45:00 +0200

    Add files via upload

--- L4/B6.c
+++ L4/B6.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int suma(int a, int b)
+{
+	int *s = 0;
+	s = (int*)malloc(sizeof(int));
+	*s = a + b;
+	return *s;
+}
+int main()
+{
+	int a, b;
+	printf("a=");
+	scanf("%d", &a);
+	printf("b=");
+	scanf("%d", &b);
+	printf("Suma este %d\n", suma(a, b));
+	system("pause");
+	return 0;
+}
