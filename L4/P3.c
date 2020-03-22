0eec1da66f95665c02a0d733c3dfa38dd370b78d
CarinaSpin <61678323+CarinaSpin@users.noreply.github.com>
2020-03-22 21:42:07 +0200

    Add files via upload

--- L4/P3.c
+++ L4/P3.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a, b, *aux;
+	printf("a=");
+	scanf("%d", &a);
+	printf("b=");
+	scanf("%d", &b);
+	aux = (int*)malloc(sizeof(int));
+	*aux = a;
+	a = b;
+	b = *aux;
+	printf("a=%d b=%d\n", a, b);
+	system("pause");
+	return 0;
+}
